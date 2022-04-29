9fc9fa664efb5f07eb2ccb74b1997b47bcbe7ea4
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2022-04-29 12:24:22 +0530

    variables in javascript

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('This is from file')
+// console.log('This is from file')
+
+let name = 'Akshat';
+console.log(name);
+
+// rules for variables
+// --cannot be a reserved keyword
+// --should be meaningful
+// --cannot start with a number (eg - 1name)
+// --cannot contain a space or hyphen (dash - )
+// --they ar case sensitive
+// --stick with camel notation
+
+let firstName = 'Akshat';
+let lastName = 'Khandelwal';
