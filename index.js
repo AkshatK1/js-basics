d24bfdb02f0f69b4aeb7757e5f3fe357506df91f
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2022-04-29 18:20:12 +0530

    Constants

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ console.log(name);
 
 let firstName = 'Akshat';
 let lastName = 'Khandelwal';
+
+//constants
+
+// let interestRate = 0.3;
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+// let allows variable value to be changed, so if varaible needs to be reassigned - use let
+// const doesnt allow variable value to change, so if variable doesnt need to  be reassigned - use const
