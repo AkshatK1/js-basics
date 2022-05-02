a98520028ecde010fccec7c27d91efbc1bb06ef4
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2022-05-02 18:56:04 +0530

    while loop

--- control_flow.js
+++ control_flow.js
@@ -43,3 +43,11 @@ for (let i = 1; i <= 5; i++) {
     if (i % 2 == 0)
         console.log(i);
 }
+
+// while loop
+let i = 1;
+while (i <= 5) {
+    if (i % 2 == 0)
+        console.log(i);
+    i++;
+}
