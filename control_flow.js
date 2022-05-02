c69be23b2387706b7462bcc817118dd01ad4697c
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2022-05-02 18:38:44 +0530

    If-else

--- control_flow.js
+++ control_flow.js
@@ -1,3 +1,16 @@
 // Conditional Statements
 // -- If...else
 // -- Switch...case
+
+// IF-ELSE
+// eg - If hour is between 6am to 12pm: Good Morning
+//      If hour is between 12pm to 6pm: Good Afternoon
+//      Otherwise:                      Good Evening
+
+let hour = 18;
+if (hour >= 6 && hour < 12)
+    console.log('Good Morning');
+else if (hour >= 12 && hour < 18)
+    console.log('Good Afternoon');
+else
+    console.log('Good Evening');
