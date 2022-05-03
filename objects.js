f7dcede2ebcbb4ed5143f10e8e7e431397b31116
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2022-05-03 17:03:09 +0530

    Object Oriented Programming (OOP) Style

---
+++ objects.js
@@ -0,0 +1,14 @@
+// Object Oriented Programming (OOP) Style
+
+const circle = {
+    radius: 2,
+    location: {
+        x: 10,
+        y: 10
+    },
+    draw: function () {
+        console.log('Draw Circle');
+    }
+};
+
+circle.draw(); //method
