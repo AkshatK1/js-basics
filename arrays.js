3f18e067700d509dfbb812615a37378ff567b2bb
Akshat Khandelwal <khandelwalakshat46@gmail.com>
2022-05-05 18:19:43 +0530

    FINDING ELEMENTS IN AN ARRAY

--- arrays.js
+++ arrays.js
@@ -10,11 +10,12 @@ numbers.push(5, 6);
 numbers.unshift(1, 2);
 
 // Middle
-numbers.splice(2, 0, 'a', 'b'); 
+numbers.splice(2, 0, 'a', 'b');
 // syntax: array.splice(index before which element to be inserted, elements to be deleted, elements to be inserted comma separated)
 console.log(numbers);
 
 // FINDING ELEMENTS IN AN ARRAY
+// Finding Primitives
 //using the same array from above
 
 numbers.indexOf(2); // it gives the index of element if its present in array, otherwise it gives -1. Its type sensitive.
@@ -26,3 +27,11 @@ numbers.splice(4, 0, 1); // adding 1 in numbers array at 4th index
 numbers.lastIndexOf(1); // this gives the last index of element in the array, in case the element is repeated
 console.log(numbers);
 console.log(numbers.lastIndexOf(1));
+
+numbers.includes(1); // this method return true if element is present in array, otherwise false
+console.log(numbers.includes(1));
+
+// there is one more variation to indexOf method, we can also specify the starting index from which the search should start
+// syntax: array.indexOf(element to be searched, starting index)
+numbers.indexOf(1, 2);
+console.log(numbers.indexOf(1, 2));
